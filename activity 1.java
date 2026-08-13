fbec2d1e0d17955ef89a6da18dcfaf5db0b4f103
Jxhan05 <johanflorendovalera@gmail.com>
2026-08-13 21:44:53 +0800

    Add files via upload

---
+++ activity 1.java
@@ -0,0 +1,15 @@
+{public class activity {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+        
+        if (sum > 10) {
+            System.out.println("Sum is greater than 10");
+        } else {
+            System.out.println("Sum is not greater than 10");
+        }
+    }
+}
